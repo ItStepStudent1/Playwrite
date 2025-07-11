e64c2c72ca8f2e4a43f703080c5e9a23412e400d
ItStepStudent1 <nadiiaonishuk@gmail.com>
2025-07-11 19:09:52 +0200

    example spec improvement

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test'; 
 test('Простой тест — проверка заголовка', async ({ page }) => { 
 await page.goto('https://example.com'); 
-await expect(page).toHaveTitle('Example Domain'); 
-}); 
-import { defineConfig } from '@playwright/test'; 
+await expect(page).toHaveTitle('Example Domain');
+console.log("This is first commit in new branch")  
+});
+
+/*import { defineConfig } from '@playwright/test'; 
 export default defineConfig({ 
     use: { 
         headless: false, // запускать тесты с видимым браузером 
@@ -11,4 +13,4 @@ export default defineConfig({
         video: 'retain-on-failure', // сохранять видео только при ошибках 
         baseURL: 'https://example.com', // можно указать базовый URL 
     }, 
-}); 
+}); */
