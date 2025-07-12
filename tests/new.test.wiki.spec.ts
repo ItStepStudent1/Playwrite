48d7341266c55a563d0c3cb5f469b7767a58bb92
ItStepStudent1 <nadiiaonishuk@gmail.com>
2025-07-12 16:02:37 +0200

    добавлено то-то

---
+++ tests/new.test.wiki.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test'; 
+import { WikiHomePage } from '../pages/WikiHomePage';  
+
+
+test('Случайная статья на Википедии — вывод заголовка', async ({ page }) => { 
+  // Шаг 1: открываем wikipedia.org и выбираем русский язык 
+    const home = new WikiHomePage(page); 
+    await home.goto(); 
+    await home.chooseLanguage();        // «Русский» по умолчанию 
+    const selectedPage = page.locator(`#n-featured`)
+    await(selectedPage).click()
+    
+
+})
